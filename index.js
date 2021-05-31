912a817c1210e73b2d566120db14d029492da4e4
sergey.dirko <sergey.dirko@auroraglobal.com>
2021-05-31 18:50:12 +0300

    game is work

--- index.js
+++ index.js
@@ -1,78 +1,85 @@
+const line_1 = { distance: 0, value: 2 };
+const line_2 = { distance: 0, value: 2 };
+const line_3 = { distance: 0, value: 2 };
+const count  = { success: 0, failed: 0 };
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.button');
+    const line1 = document.querySelector('.arrow_left');
+    const line2 = document.querySelector('.arrow_up');
+    const line3 = document.querySelector('.arrow_right');
     const sCount = document.querySelector('.counter_success').children.item(0);
     const fCount = document.querySelector('.counter_failed').children.item(0);
+    let interval1, interval2, interval3;
 
-    button.addEventListener('click', start.bind(null, button));
     document.addEventListener('keydown', event => {
-        createKeyBehavior(event, render.bind(null, sCount, fCount));
+        createKeyBehavior(event, renderCount);
     });
-});
+    button.addEventListener('click', start);
 
-const count = {
-    success: 0,
-    failed: 0,
-}
-const line_1 = {
-    distance: 0,
-    value: 80,
-};
-const line_2 = {
-    distance: 0,
-    value: 80,
-};
-const line_3 = {
-    distance: 0,
-    value: 80,
-};
-
-function createKeyBehavior(event, recount) {
-    if (event.code === 'ArrowLeft') {
-        if (line_1.distance < line_1.value) {
-            count.success++;
-        } else {
-            count.failed++;
+    function createKeyBehavior(event, recount) {
+        if (event.code === 'ArrowLeft') {
+            if (line_1.distance === line_1.value) {
+                count.success++;
+                clearInterval(interval1);
+                interval1 = startLine(line_1, line1, 1000);
+            } else {
+                count.failed++;
+            }
+        } else if (event.code === 'ArrowUp') {
+            if (line_2.distance === line_2.value) {
+                count.success++;
+                clearInterval(interval2);
+                interval2 = startLine(line_2, line2, 1000);
+            } else {
+                count.failed++;
+            }
+        } else if (event.code === 'ArrowRight') {
+            if (line_3.distance === line_3.value) {
+                count.success++;
+                clearInterval(interval2);
+                interval3 = startLine(line_3, line3, 1000);
+            } else {
+                count.failed++;
+            }
         }
-    } else if (event.code === 'ArrowUp') {
-        if (line_2.distance > line_2.value) {
-            count.success++;
-        } else {
-            count.failed++;
-        }
-    } else if (event.code === 'ArrowRight') {
-        if (line_3.distance > line_3.value) {
-            count.success++;
-        } else {
-            count.failed++;
-        }
+        recount();
     }
-    recount();
-}
 
-function startLine(line, speed) {
-    const interval = setInterval(() => {
-        console.log(line.distance);
-        if (line.distance === 100) {
-            clearInterval(interval);
-            startLine(line, speed);
-        }
-        line.distance++; // TODO check
-    }, speed);
-}
+    function startLine(data, line, speed) {
+        const interval = setInterval(() => {
+            if (data.distance > 10) {
+                data.distance = 0;
+                clearInterval(interval);
+                startLine(data, line, speed);
+            }
+            data.distance++; // TODO check
 
-function linePass() {
-    startLine(line_1, 1000);
-    startLine(line_2, 2000);
-    startLine(line_3, 5000);
-}
+            if (data.value === data.distance) {
+                line.style.backgroundColor = '#2cfc2c';
+            } else {
+                if (line.style.backgroundColor !== '#ff3030') {
+                    line.style.backgroundColor = '#ff3030';
+                }
+            }
+        }, speed);
+        return interval;
+    }
 
-function render(success, failed) {
-    success.innerText = count.success;
-    failed.innerText = count.failed;
-}
+    function linePass() {
+        interval1 = startLine(line_1, line1, 1000);
+        interval2 = startLine(line_2, line2, 2000);
+        interval3 = startLine(line_3, line3, 2500);
+    }
 
-function start(button) {
-    linePass();
-    button.removeEventListener('click', start);
-    button.remove();
-}
+    function renderCount() {
+        sCount.innerText = count.success;
+        fCount.innerText = count.failed;
+    }
+
+    function start() {
+        linePass();
+        button.removeEventListener('click', start);
+        button.remove();
+    }
+});
